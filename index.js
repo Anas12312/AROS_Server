21d93d04668780e625c41d5a43a03c9a12ff08f2
anas12312 <anashesham000@gmail.com>
2023-12-02 14:54:10 +0200

    env variables

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Server is Running")
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`Server is running on port:${port}`)
+})
